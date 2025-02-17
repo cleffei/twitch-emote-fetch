b958445b68d61f4215f9fb616a7ed73e03a7e2c1
Adam Herdman <cleffei@hotmail.com>
2025-02-17 18:05:56 +0000

    Added size option radio buttons

--- src/assets/components/EmoteFetcher.tsx
+++ src/assets/components/EmoteFetcher.tsx
@@ -4,6 +4,7 @@ import { fetchEmotes } from '../../api';
 const EmoteFetcher = () => {
   const [channelId, setChannelId] = useState('');
   const [emotes, setEmotes] = useState<any[]>([]);
+  const [size, setSize] = useState('2x');
 
   const handleFetchEmotes = async () => {
     if (!channelId) {
@@ -27,14 +28,42 @@ const EmoteFetcher = () => {
           }
         />
         <button onClick={handleFetchEmotes}>View Channel's Emotes</button>
+        <input
+          type="radio"
+          id="1xBtn"
+          name="size"
+          value="1x"
+          checked={size === '1x'}
+          onChange={(e) => setSize(e.target.value)}
+        />
+        <label htmlFor="1xBtn">1x Size</label>
+        <input
+          type="radio"
+          id="2xBtn"
+          name="size"
+          value="2x"
+          checked={size === '2x'}
+          onChange={(e) => setSize(e.target.value)}
+        />
+        <label htmlFor="2xBtn">2x Size</label>
+        <input
+          type="radio"
+          id="4xBtn"
+          name="size"
+          value="4x"
+          checked={size === '4x'}
+          onChange={(e) => setSize(e.target.value)}
+        />
+        <label htmlFor="4xBtn">4x Size</label>
       </div>
 
       <div>
         {emotes.map((emote: any) => {
           const isAnimated = emote.format.includes('animated');
+
           const imageUrl = isAnimated
-            ? emote.images.url_2x.replace('static/', 'animated/')
-            : emote.images.url_2x;
+            ? emote.images[`url_${size}`].replace('static/', 'animated/')
+            : emote.images[`url_${size}`];
 
           return <img key={emote.id} src={imageUrl} alt={emote.name} />;
         })}
