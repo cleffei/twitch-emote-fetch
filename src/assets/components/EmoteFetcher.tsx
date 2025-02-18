c85d7652f50f21b4e81fbeb471b9bc3f7378cdf4
Adam Herdman <cleffei@hotmail.com>
2025-02-18 11:15:25 +0000

    Centred output

--- src/assets/components/EmoteFetcher.tsx
+++ src/assets/components/EmoteFetcher.tsx
@@ -98,7 +98,7 @@ const EmoteFetcher = () => {
 
       <ToastContainer />
 
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 mt-4">
+      <div className=" max-w-2/3 mx-auto grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 mt-4">
         {emotes.map((emote: any) => {
           const isAnimated = emote.format.includes('animated');
           const imageUrl = isAnimated
