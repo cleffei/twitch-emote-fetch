21156b3cfe16f9824c34643a038945b105f05074
Adam Herdman <cleffei@hotmail.com>
2025-02-24 17:01:02 +0000

    logging

--- src/api.ts
+++ src/api.ts
@@ -5,6 +5,7 @@ export const fetchEmotesByChannelName = async (channelName: string) => {
     const response = await axios.get(
       `/api/fetchEmotes?channelName=${channelName}`
     );
+    console.log(response.data.data);
     return response.data.data || [];
   } catch (error) {
     console.error('Error occurred fetching emotes\n', error);
