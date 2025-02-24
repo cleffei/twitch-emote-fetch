ada7002839b306570db601bacf39615657ffb5b2
Adam Herdman <cleffei@hotmail.com>
2025-02-24 15:58:29 +0000

    Netlify function for API key security

---
+++ netlify/functions/fetchEmotes.ts
@@ -0,0 +1,75 @@
+import axios from 'axios';
+import { Handler } from '@netlify/functions';
+
+const clientId = process.env.TWITCH_CLIENT_ID;
+const accessToken = process.env.TWITCH_ACCESS_TOKEN;
+
+const fetchUserId = async (channelName: string) => {
+  try {
+    const response = await axios.get(
+      `https://api.twitch.tv/helix/users?login=${channelName}`,
+      {
+        headers: {
+          'Client-Id': clientId,
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+    if (response.data.data.length === 0) {
+      return null;
+    } else {
+      return response.data.data[0].id;
+    }
+  } catch (error) {
+    console.error('Error occurred fetching channel ID\n', error);
+    return null;
+  }
+};
+
+const fetchEmotes = async (channelId: string) => {
+  try {
+    const response = await axios.get(
+      `https://api.twitch.tv/helix/chat/emotes?broadcaster_id=${channelId}`,
+      {
+        headers: {
+          'Client-Id': clientId,
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+    if (response.data.data.length === 0) {
+      return null;
+    } else {
+      return response.data.data;
+    }
+  } catch (error) {
+    console.error('Error occurred fetching emotes\n', error);
+    return [];
+  }
+};
+
+const handler: Handler = async (event, context) => {
+  const channelName = event.queryStringParameters?.channelName;
+  if (!channelName) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: 'Channel name is required' }),
+    };
+  }
+
+  const userId = await fetchUserId(channelName);
+  if (!userId) {
+    return {
+      statusCode: 404,
+      body: JSON.stringify({ message: 'Channel not found' }),
+    };
+  }
+
+  const emotes = await fetchEmotes(userId);
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ emotes }),
+  };
+};
+
+export { handler };
--- src/api.ts
+++ src/api.ts
@@ -1,57 +1,13 @@
 import axios from 'axios';
 
-const clientId = import.meta.env.VITE_CLIENT_ID;
-const accessToken = import.meta.env.VITE_ACCESS_TOKEN;
-
-export const fetchUserId = async (channelName: string) => {
+export const fetchEmotesByChannelName = async (channelName: string) => {
   try {
     const response = await axios.get(
-      `https://api.twitch.tv/helix/users?login=${channelName}`,
-      {
-        headers: {
-          'Client-Id': clientId,
-          Authorization: `Bearer ${accessToken}`,
-        },
-      }
+      `/api/fetchEmotes?channelName=${channelName}`
     );
-    if (response.data.data.length === 0) {
-      return null;
-    } else {
-      return response.data.data[0].id;
-    }
-  } catch (error) {
-    console.error('Error occurred fetching channel ID\n', error);
-    return null;
-  }
-};
-
-export const fetchEmotes = async (channelId: string) => {
-  try {
-    const response = await axios.get(
-      `https://api.twitch.tv/helix/chat/emotes?broadcaster_id=${channelId}`,
-      {
-        headers: {
-          'Client-Id': clientId,
-          Authorization: `Bearer ${accessToken}`,
-        },
-      }
-    );
-    if (response.data.data.length === 0) {
-      return null;
-    } else {
-      return response.data.data;
-    }
+    return response.data.data;
   } catch (error) {
     console.error('Error occurred fetching emotes\n', error);
     return [];
-  }
-};
-
-export const fetchEmotesByChannelName = async (channelName: string) => {
-  const userId = await fetchUserId(channelName);
-  if (!userId) {
-    return null;
-  } else {
-    return fetchEmotes(userId);
   }
 };
