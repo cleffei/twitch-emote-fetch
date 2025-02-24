a2d78a4c0a510666fa1588f5b0ce7319568544f8
Adam Herdman <cleffei@hotmail.com>
2025-02-24 17:16:14 +0000

    Removed logging

--- src/api.ts
+++ src/api.ts
@@ -5,7 +5,6 @@ export const fetchEmotesByChannelName = async (channelName: string) => {
     const response = await axios.get(
       `https://twitch-emote-fetcher.netlify.app/.netlify/functions/fetchEmotes?channelName=${channelName}`
     );
-    console.log(response);
     return response.data.emotes || [];
   } catch (error) {
     console.error('Error occurred fetching emotes\n', error);
